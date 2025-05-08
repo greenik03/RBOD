3fb66584ba294eb5a5cd072f136d3979a50a2cb6
greenik03 <175439793+greenik03@users.noreply.github.com>
2025-05-08 23:20:28 +0200

    Added code to RBOD.java
    - Merged 'int getPhrasesCount()' and 'String readPhraseFromFile(int)' into 'List<String> readPhrasesFromFile()'
    - Added code to 'String readTokenFromFile()'
    - Renamed 'token' File to 'discordToken' to not be confused with 'token' String in main

--- src/main/java/RBOD.java
+++ src/main/java/RBOD.java
@@ -4,21 +4,52 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Random;
+
 public class RBOD extends ListenerAdapter {
+    static File discordToken = new File("assets/token.txt"),
+    phrases = new File("assets/phrases.txt");
+    static Random rng = new Random();
+
     public static void main(String[] args) {
-
+        String token = readTokenFromFile();
+        List<String> phrasesList = readPhrasesFromFile();
+        int phraseIndex = rng.nextInt(0, phrasesList.size());
+//        System.out.printf("%d: %s", phraseIndex, phrasesList.get(phraseIndex));
     }
 
-    static void readPhrasesFromFile() {
-
+    static List<String> readPhrasesFromFile() {
+        if (phrases.exists() && phrases.canRead()) {
+            try {
+                return Files.readAllLines(phrases.toPath(), StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {
+            throw new RuntimeException("phrases.txt does not exist or cannot be read. Read guide.md for more info.");
+        }
     }
 
-    static void readTokenFromFile() {
-
-    }
-
-    static void getPhrasesCount() {
-
+    static String readTokenFromFile() {
+        if (discordToken.exists() && discordToken.canRead()) {
+            try {
+                return Files.readString(discordToken.toPath(), StandardCharsets.UTF_8)
+                        .trim();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {
+            throw new RuntimeException("token.txt does not exist or cannot be read. Read guide.md for more info.");
+        }
     }
 
     @Override
