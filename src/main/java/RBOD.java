c9883d1a04dfde72437272a0cecd4dd5dce533d1
greenik03 <175439793+greenik03@users.noreply.github.com>
2025-05-08 21:30:26 +0200

    Created necessary files

---
+++ src/main/java/RBOD.java
@@ -0,0 +1,33 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class RBOD extends ListenerAdapter {
+    public static void main(String[] args) {
+
+    }
+
+    static void readPhrasesFromFile() {
+
+    }
+
+    static void readTokenFromFile() {
+
+    }
+
+    static void getPhrasesCount() {
+
+    }
+
+    @Override
+    public void onReady(ReadyEvent event) {
+        super.onReady(event);
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        super.onMessageReceived(event);
+    }
+}
