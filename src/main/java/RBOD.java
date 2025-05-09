3b350558a311e698d6a60ef1b83d8dc016387e83
greenik03 <175439793+greenik03@users.noreply.github.com>
2025-05-09 03:56:02 +0200

    Update to RBOD.java
    - void 'onGuildJoin(GuildJoinEvent)' override
    - Replaced String 'readTokenFromFile()' with String 'readSingleLineFile(File)'
    - Added static variables: EnumSet<GatewayIntent> 'intents', File 'appID', booleans 'reactOnName' and 'reactOnReply'
    - Updated code on main (still WIP)

--- src/main/java/RBOD.java
+++ src/main/java/RBOD.java
@@ -1,30 +1,70 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.events.session.ShutdownEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.charset.StandardCharsets;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Random;
 
 public class RBOD extends ListenerAdapter {
+    // Initialize variables
+    static EnumSet<GatewayIntent> intents = EnumSet.of(
+        GatewayIntent.GUILD_MESSAGES,
+        GatewayIntent.DIRECT_MESSAGES,
+        GatewayIntent.MESSAGE_CONTENT,
+        GatewayIntent.GUILD_MESSAGE_REACTIONS,
+        GatewayIntent.DIRECT_MESSAGE_REACTIONS
+    );
     static File discordToken = new File("assets/token.txt"),
-    phrases = new File("assets/phrases.txt");
+    phrases = new File("assets/phrases.txt"),
+    appID = new File("assets/applicationID.txt");
     static Random rng = new Random();
+    static boolean reactOnName = false,
+            reactOnReply = false;
 
+    // Start of main method
     public static void main(String[] args) {
-        String token = readTokenFromFile();
+        String token = readSingleLineFile(discordToken);
         List<String> phrasesList = readPhrasesFromFile();
         int phraseIndex = rng.nextInt(0, phrasesList.size());
+        JDA jda = JDABuilder.createLight(token, intents)
+                .addEventListeners(new RBOD())
+                .setActivity(Activity.customStatus("It's reacting time!"))
+                .build();
 //        System.out.printf("%d: %s", phraseIndex, phrasesList.get(phraseIndex));
-    }
 
+        CommandListUpdateAction commands = jda.updateCommands();
+        //TODO: Add options for response activations on name mention and on reply through slash commands. Direct mentions are always on.
+
+        try {
+                jda.awaitReady();
+        }
+        catch (Exception e) {
+            System.err.println("Something went wrong while starting the bot.");
+            throw new RuntimeException(e);
+        }
+
+    }
+    // End of main method
+
+    // Read text in assets/phrases.txt, used as variable in main
     static List<String> readPhrasesFromFile() {
         if (phrases.exists() && phrases.canRead()) {
             try {
@@ -34,31 +74,56 @@ public class RBOD extends ListenerAdapter {
             }
         }
         else {
-            throw new RuntimeException("phrases.txt does not exist or cannot be read. Read guide.md for more info.");
+            throw new RuntimeException("phrases.txt does not exist or cannot be read. Read assets/guide.md for more info.");
         }
     }
 
-    static String readTokenFromFile() {
-        if (discordToken.exists() && discordToken.canRead()) {
+    // Read token from assets/token.txt or bot's ID from assets/applicationID.txt
+    static String readSingleLineFile(File file) {
+        if (file.exists() && file.canRead()) {
             try {
-                return Files.readString(discordToken.toPath(), StandardCharsets.UTF_8)
+                return Files.readString(file.toPath(), StandardCharsets.UTF_8)
                         .trim();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
         else {
-            throw new RuntimeException("token.txt does not exist or cannot be read. Read guide.md for more info.");
+            throw new RuntimeException(file.getAbsolutePath() + " does not exist or cannot be read. Read assets/guide.md for more info.");
         }
     }
 
     @Override
-    public void onReady(ReadyEvent event) {
+    public void onReady(@NotNull ReadyEvent event) {
         super.onReady(event);
     }
 
     @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         super.onMessageReceived(event);
+    }
+
+    @Override
+    public void onShutdown(@NotNull ShutdownEvent event) {
+        super.onShutdown(event);
+    }
+
+    // Message when bot joins a guild/server
+    @Override
+    public void onGuildJoin(GuildJoinEvent event) {
+        String welcomeMessage = "Hi! I'm ReactBot! @ me for a cool reaction! Use `/toggle` for additional ways to interact with me.";
+        TextChannel channel = event.getGuild().getSystemChannel();
+        if (channel == null || !channel.canTalk()) {
+            for (TextChannel textChannel : event.getGuild().getTextChannels()) {
+                if (textChannel.canTalk()) {
+                    channel = textChannel;
+                    break;
+                }
+            }
+        }
+        if (channel != null) {
+            channel.sendMessage(welcomeMessage)
+                    .queue();
+        }
     }
 }
