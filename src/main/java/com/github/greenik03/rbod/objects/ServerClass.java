ca971feded9dfd1ccb3a055aea559cc1c01a5f47
greenik03 <175439793+greenik03@users.noreply.github.com>
2025-07-16 07:07:21 +0200

    Update 'toString()' method in ServerClass.java

--- src/main/java/com/github/greenik03/rbod/objects/ServerClass.java
+++ src/main/java/com/github/greenik03/rbod/objects/ServerClass.java
@@ -1,6 +1,5 @@
 package com.github.greenik03.rbod.objects;
 
-// Used to be Serializable, only used to show server info in the CLI (for now)
 public class ServerClass {
     String serverId;
     SettingsObj settings;
@@ -9,12 +8,9 @@ public class ServerClass {
         this.serverId = serverId;
         this.settings = settings;
     }
-    //TODO: Change string format
+
     @Override
     public String toString() {
-        return "Server {" +
-                "serverId='" + serverId + '\'' +
-                ", settings=" + settings +
-                '}';
+        return String.format("%s:\n%s\n%s", serverId, settings.getNames(),  settings);
     }
 }
